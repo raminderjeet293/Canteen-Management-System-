d983c0b87b8b7f0450cfa82180b212501c194e5d
Akhilaknair <akhilaknair24@gmail.com>
2025-02-13 14:12:23 +0530

    minor changes

--- frontend/src/pages/Cart/CartPage.js
+++ frontend/src/pages/Cart/CartPage.js
@@ -1,34 +1,38 @@
-import React from 'react'
-import classes from './cartPage.module.css'
-import { useCart } from '../../hooks/useCart';
-import Title from '../../components/Title/Title';
-import { Link } from 'react-router-dom';
-import Price from '../../components/Price/Price';
+import React from "react";
+import classes from "./cartPage.module.css";
+import { useCart } from "../../hooks/useCart";
+import Title from "../../components/Title/Title";
+import { Link } from "react-router-dom";
+import Price from "../../components/Price/Price";
 export default function CartPage() {
-
-    const {cart,removeFromCart,changeQuantity}=useCart();
+  const { cart, removeFromCart, changeQuantity } = useCart();
 
   return (
     <>
-     <Title title='Cart Page' margin="1.5rem 0 0 2.5rem"/>
-     {
-     cart && cart.items.length >0 && 
+      <Title title="Cart Page" margin="1.5rem 0 0 2.5rem" />
+      {cart && cart.items.length > 0 && (
         <div className={classes.container}>
           <ul className={classes.list}>
-            {
-               cart.items.map(item=><li key={item.food.id}>
+            {cart.items.map((item) => (
+              <li key={item.food.id}>
                 <div>
-                  <img src={`/foods/${item.food.imageUrl}`} alt={item.food.name}>
-                  </img>
+                  <img
+                    src={`/foods/${item.food.imageUrl}`}
+                    alt={item.food.name}
+                  ></img>
                 </div>
 
                 <div>
                   <Link to={`/food/${item.food.id}`}>{item.food.name}</Link>
                 </div>
 
-
                 <div>
-                  <select value={item.quantity} onChange={e=>changeQuantity(item,Number(e.target.value))}>
+                  <select
+                    value={item.quantity}
+                    onChange={(e) =>
+                      changeQuantity(item, Number(e.target.value))
+                    }
+                  >
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
@@ -39,39 +43,35 @@ export default function CartPage() {
                     <option>8</option>
                     <option>9</option>
                     <option>10</option>
-                  
                   </select>
                 </div>
 
+                <div>
+                  <Price price={item.price} />
+                </div>
 
-               <div>
-                <Price price={item.price}/>
-               </div>
-
-
-              <button className={classes.remove_button} onClick={()=>removeFromCart(item.food.id)}>Remove</button>
-
-               </li>)
-            }
+                <button
+                  className={classes.remove_button}
+                  onClick={() => removeFromCart(item.food.id)}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
           </ul>
 
-   <div className={classes.checkout}>
-      <div>
-        <div className={classes.foods_count}>{cart.totalCount}</div>
-        <div className={classes.total_price}>
-           <Price price={cart.totalPrice}/>
+          <div className={classes.checkout}>
+            <div>
+              <div className={classes.foods_count}>{cart.totalCount}</div>
+              <div className={classes.total_price}>
+                <Price price={cart.totalPrice} />
+              </div>
+            </div>
+
+            <Link to="/checkout">Proceed To Checkout</Link>
+          </div>
         </div>
-      </div>
-
-       <Link to='/checkout'>Proceed To Checkout</Link>
-
-
-   </div>
-
-
-
-        </div>
-     }
+      )}
     </>
-  )
+  );
 }
