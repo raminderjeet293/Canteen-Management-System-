3f33927d41351c5c4c2631db83ceaa525e6daf0e
raminderjeet293 <raminderjeetkaur02@gmail.com>
2025-02-17 10:09:00 +0530

    Not Found Page added

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./search.module.css";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -6,6 +6,11 @@ export default function Search() {
   const [term, setTerm] = useState("");
   const navigate = useNavigate();
   const { searchTerm } = useParams();
+
+  useEffect(() => {
+    setTerm(searchTerm ?? "");
+  }, [searchTerm]);
+
   const search = async () => {
     term ? navigate("/search/" + term) : navigate("/");
   };
@@ -16,7 +21,7 @@ export default function Search() {
         placeholder="search Food"
         onChange={(e) => setTerm(e.target.value)}
         onKeyUp={(e) => e.key === "Enter" && search()}
-        defaultValue={searchTerm}
+        value={term}
       ></input>
       <button onClick={search}>Search</button>
     </div>
