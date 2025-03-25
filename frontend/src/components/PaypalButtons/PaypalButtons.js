8c296748e1f5b8208f14d5efbda09a97260e3363
raminderjeet293 <raminderjeetkaur02@gmail.com>
2025-03-25 21:29:31 +0530

    paypal folder

--- frontend/src/components/PaypalButtons/PaypalButtons.js
+++ frontend/src/components/PaypalButtons/PaypalButtons.js
@@ -1,6 +1,5 @@
-import { PayPalScriptProvider } from '@paypal/react-paypal-js'
-import React from 'react'
+import React from "react";
 
 export default function PaypalButtons() {
-  return <PayPalScriptProvider ></PayPalScriptProvider>
+  return <div>Payment</div>;
 }
